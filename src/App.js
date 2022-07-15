6fd31f12b3edf662a0828b8b9eac10b871e29420
NadiiaHonchar <17nmg82@gmail.com>
2022-07-16 00:03:54 +0300

    addCalculate

--- src/App.js
+++ src/App.js
@@ -6,30 +6,97 @@ import SearchForm from "./Components/SearchForm";
 import axios from "axios";
 
 function App() {
-  // const API_KEY = "81ed5d216ebe4d362cf78679df36331bacbc9e5c";
-
   const [count1, setCount1] = useState(1);
   const [count2, setCount2] = useState(1);
-  const [currency1, setCurrency1] = useState("UHA");
-  const [currency2, setCurrency2] = useState("UHA");
+  const [currency1, setCurrency1] = useState("UAH");
+  const [currency2, setCurrency2] = useState("UAH");
   const [price, setPrice] = useState([]);
-  // const [stats, setStates] = useState([]);
 
   useEffect(() => {
-    // axios.get("https://api.minfin.com.ua/mb/81ed5d216ebe4d362cf78679df36331bacbc9e5c/")
-    axios.get("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json")
-      .then((responce) => {setPrice(responce.data);
+    axios
+      .get(
+        "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json"
+      )
+      .then((responce) => {
+        setPrice(
+          [
+            {
+              r030: 980,
+              txt: "Українська гривня",
+              rate: 1,
+              cc: "UAH",
+              exchangedate: "18.07.2022",
+            },
+          ].concat(responce.data)
+        );
       });
   }, []);
 
-  const newStates =price.map(({cc})=> cc);
+  useEffect(() => {
+    if (!!price) {
+      handleCount1Change(1);
+    }
+  }, [price]);
 
+  function format(number) {
+    return number.toFixed(4);
+  }
+
+  function receiveRate(currency) {
+    let rates = 0;
+    price.map((item) => {
+      if (item.cc === currency) {
+        rates = item.rate;
+      }
+    });
+    return rates;
+  }
+
+  function handleCount1Change(count1) {
+    let rates1 = receiveRate(currency1);
+    let rates2 = receiveRate(currency2);
+    setCount2(format((count1 * rates2) / rates1));
+    setCount1(count1);
+  }
+
+  function handleCurrency1Change(currency1) {
+    let rates1 = receiveRate(currency1);
+    let rates2 = receiveRate(currency2);
+    setCount2(format((count1 * rates2) / rates1));
+    setCurrency1(currency1);
+  }
+
+  function handleCount2Change(count2) {
+    let rates1 = receiveRate(currency1);
+    let rates2 = receiveRate(currency2);
+    setCount1(format((count2 * rates1) / rates2));
+    setCount2(count2);
+  }
+
+  function handleCurrency2Change(currency2) {
+    let rates1 = receiveRate(currency1);
+    let rates2 = receiveRate(currency2);
+    setCount1(format((count2 * rates1) / rates2));
+    setCurrency2(currency2);
+  }
 
   return (
     <>
       <Header />
-      <SearchForm currencies={newStates} count={count1} currency={currency1} />
-      <SearchForm currencies={newStates} count={count2} currency={currency2} />
+      <SearchForm
+        currencies={price.map(({ cc }) => cc)}
+        count={count1}
+        currency={currency1}
+        onCountChange={handleCount1Change}
+        onCurrencyChange={handleCurrency1Change}
+      />
+      <SearchForm
+        currencies={price.map(({ cc }) => cc)}
+        count={count2}
+        currency={currency2}
+        onCountChange={handleCount2Change}
+        onCurrencyChange={handleCurrency2Change}
+      />
     </>
   );
 }
--- src/Components/SearchForm/SearchForm.js
+++ src/Components/SearchForm/SearchForm.js
@@ -1,58 +1,29 @@
-import { useState, useEffect } from "react";
 import style from "./SearchForm.module.css";
 
-export default function SearchForm({count, currency, currencies}) {
-
-  // const newStates =currencies.map(({cc, rate})=> ({
-  //     [cc]:rate,
-  //   }))
-  //   console.log(newStates);
-
+export default function SearchForm({
+  count,
+  currency,
+  currencies,
+  onCountChange,
+  onCurrencyChange,
+}) {
   return (
     <>
       <div className="formInput">
-        <input type="text" value={count} />
-        <select value={currency}>
-          {/* {console.log(Object.values(currencies))} */}
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => onCountChange(e.target.value)}
+        />
+        <select
+          value={currency}
+          onChange={(e) => onCurrencyChange(e.target.value)}
+        >
           {currencies.map((item) => (
-            <option value={item}>{item}</option>
+            <option value={item} key={item}>{item}</option>
           ))}
         </select>
       </div>
-      {/* <form className={style.form}>
-        <h2 className={style.text}>Exchange</h2>
-        <input
-          type="numeric"
-          name="exchange"
-          value={exchange}
-          onChange={handleInputChange}
-          className={style.input}
-          required
-        />
-        <select name="selectExchange" onChange={handleInputChange}>
-          <option defaultValue="UAH">
-            UAH
-          </option>
-          <option value="USD">USD</option>
-          <option value="EUR">EUR</option>
-        </select>
-        <h2 className={style.text}>Receipt</h2>
-        <input
-          type="numeric"
-          name="receipt"
-          value={receipt}
-          onChange={handleInputChange}
-          className={style.input}
-          required
-        />
-        <select name="selectReceipt" onChange={handleInputChange}>
-          <option defaultValue="UAH">
-            UAH
-          </option>
-          <option value="USD">USD</option>
-          <option value="EUR">EUR</option>
-        </select>
-      </form> */}
     </>
   );
 }
